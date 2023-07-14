1f2141746ab1375894d16a367874d7fda98cf95f
Hridoy <106481465+Hridoy0016@users.noreply.github.com>
2023-07-14 20:58:48 +0530

    Arashi V1.0.0

--- BaileysJs/lib/Types/GroupMetadata.d.ts
+++ BaileysJs/lib/Types/GroupMetadata.d.ts
@@ -1,43 +1,42 @@
-import { Contact } from './Contact'
-
-export type GroupParticipant = (Contact & { isAdmin?: boolean, isSuperAdmin?: boolean, admin?: 'admin' | 'superadmin' | null })
-
-export type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote'
-
+import { Contact } from './Contact';
+export declare type GroupParticipant = (Contact & {
+    isAdmin?: boolean;
+    isSuperAdmin?: boolean;
+    admin?: 'admin' | 'superadmin' | null;
+});
+export declare type ParticipantAction = 'add' | 'remove' | 'promote' | 'demote';
 export interface GroupMetadata {
-    id: string
-    owner: string | undefined
-    subject: string
+    id: string;
+    owner: string | undefined;
+    subject: string;
     /** group subject owner */
-    subjectOwner?: string
+    subjectOwner?: string;
     /** group subject modification date */
-    subjectTime?: number
-    creation?: number
-    desc?: string
-    descOwner?: string
-    descId?: string
+    subjectTime?: number;
+    creation?: number;
+    desc?: string;
+    descOwner?: string;
+    descId?: string;
     /** is set when the group only allows admins to change group settings */
-    restrict?: boolean
+    restrict?: boolean;
     /** is set when the group only allows admins to write messages */
-    announce?: boolean
+    announce?: boolean;
     /** number of group participants */
-    size?: number
-    // Baileys modified array
-    participants: GroupParticipant[]
-    ephemeralDuration?: number
-    inviteCode?: string
+    size?: number;
+    participants: GroupParticipant[];
+    ephemeralDuration?: number;
+    inviteCode?: string;
 }
-
-
 export interface WAGroupCreateResponse {
-    status: number
-    gid?: string
-    participants?: [{ [key: string]: {} }]
+    status: number;
+    gid?: string;
+    participants?: [{
+        [key: string]: {};
+    }];
 }
-
 export interface GroupModificationResponse {
-    status: number
-    participants?: { 
-        [key: string]: {} 
+    status: number;
+    participants?: {
+        [key: string]: {};
     };
 }
