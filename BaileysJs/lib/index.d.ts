aa5164653b8221aafbd839e71276937387de2de4
Hridoy <106481465+Hridoy0016@users.noreply.github.com>
2023-07-14 20:14:32 +0530

    Arashi V1.0.0

--- BaileysJs/lib/index.d.ts
+++ BaileysJs/lib/index.d.ts
@@ -1,12 +1,9 @@
-import makeWASocket from './Socket'
-
-export * from '../WAProto'
-export * from './Utils'
-export * from './Types'
-export * from './Store'
-export * from './Defaults'
-export * from './WABinary'
-
-export type WASocket = ReturnType<typeof makeWASocket>
-export { makeWASocket }
-export default makeWASocket
+import makeWASocket from './Socket';
+export * from '../WAProto';
+export * from './Utils';
+export * from './Types';
+export * from './Store';
+export * from './Defaults';
+export * from './WABinary';
+export declare type WASocket = ReturnType<typeof makeWASocket>;
+export default makeWASocket;
