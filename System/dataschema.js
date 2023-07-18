e4cab839746fce2d2f8011f5a63579804d9d962b
Hridoy <106481465+Hridoy0016@users.noreply.github.com>
2023-07-18 10:23:46 +0530

    Update dataschema.js

--- System/dataschema.js
+++ System/dataschema.js
@@ -1,4 +1,11 @@
+require("../config.js");
+const config = require('../config');
 const mongoose = require("mongoose");
+const eco = require('discord-mongoose-economy');
+
+const db1= mongoose.createConnection(config.mongodb);
+const db2= mongoose.createConnection(config.mongodb);
+
 
 const GroupSchema = new mongoose.Schema({
   id: { type: String,  unique: true ,required: true },
