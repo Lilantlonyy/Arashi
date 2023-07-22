aa96aaff3bda17b3b646225c0a742f186273626d
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2023-07-22 12:45:04 +0530

    Update Function2.js

--- System/Function2.js
+++ System/Function2.js
@@ -1,4 +1,4 @@
-const { proto, delay, getContentType } = require("../BaileysJS/lib");
+const { proto, delay, getContentType } = require("baileys");
 const chalk = require("chalk");
 const fs = require("fs");
 const { unlink } = require("fs").promises;
